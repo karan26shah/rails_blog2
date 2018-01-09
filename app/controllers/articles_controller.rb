9ca67573c22f4b84cdd3261b3ac698f9d3bfdd25
karan <karanpankajshah@gmail.com>
2018-01-09 13:30:50 +0530

    comments added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,12 @@ end
 def new
   @article = Article.new
 end
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
  def edit
   @article = Article.find(params[:id])
 end
---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,24 @@
+class TopicsController < ApplicationController
+ def index
+    @topics = Topic.all
+  end
+def show
+    @topic = Topic.find(params[:id])
+end
+def new
+  @topic = Topic.new
+end
+def create
+  @topic = Topic.new(topic_params)
+ 
+  if @topic.save
+    redirect_to @topic
+  else
+    render 'new'
+  end
+end
+private
+def topic_params
+    params.require(:topic).permit(:title)
+end
+end
